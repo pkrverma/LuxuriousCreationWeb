9c191167fc76b43cb3bfaa4670bb8d5b2d50be4b
Pulkit Verma <pulkit18012003@gmail.com>
2024-04-14 14:57:18 +0530

    Contact form message box update

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,57 +57,63 @@ function sendMail() {
   const email = document.querySelector('input[type="email"]');
   const otpbutton = document.querySelector('#otpbtn');
 
-  // generate OTP
-  const otpLength = 6;
-  const randomNumber = Math.floor(Math.random() * 1000000);
-  const randomString = randomNumber.toString();
-  const paddedString = randomString.padStart(otpLength, '0');
-  otpVal = paddedString.substring(0, otpLength);
-  console.log("sendMail(): " + otpVal);
-  console.log(email.value);
+  if (email.value === '') {
+    alert("Please fill the Email Address to get OTP!");
+  } else {
+    // generate OTP
+    const otpLength = 6;
+    const randomNumber = Math.floor(Math.random() * 1000000);
+    const randomString = randomNumber.toString();
+    const paddedString = randomString.padStart(otpLength, '0');
+    otpVal = paddedString.substring(0, otpLength);
+    console.log("sendMail(): " + otpVal);
 
-  let emailBody = `
+    let emailBody = `
+    <img src = "https://drive.google.com/file/d/1H5M_K4w5J7vwGxpCllf_wksM5JqCvYAY/view?usp=drive_link" style="height:1rem; width:1rem;" alt = "LC Logo">
     <h3>Luxurious Creation: Visualise Around Yourself With AR</h3>
     <h5>Make Your Living Comfortable</h5>
     <br>
     <p>Your OTP for email verifiation is: <strong>${otpVal}</strong>.</p>
     <p>Thank You</p>`;
 
-  Email.send({
-    SecureToken: "314209bb-7b82-4dd5-919d-7d9286a7d706",
-    To: email.value,
-    From: "luxuriouscreation02@gmail.com",
-    Subject: "Email Verification OTP from Luxurious Creation",
-    Body: emailBody,
-  }).then(
-    message => {
-      if (message === "OK") {
-        document.getElementById('otpdiv').style.display = "block";
-        document.getElementById('verifydiv').style.display = "block";
-        setTimeout(() => {
-          alert("OTP sent to " + email.value)
-          otpbutton.innerText = "Resend OTP"
-        }, 2000);
-      } else {
-        alert("Error Occured!")
+    Email.send({
+      SecureToken: "314209bb-7b82-4dd5-919d-7d9286a7d706",
+      To: email.value,
+      From: "luxuriouscreation02@gmail.com",
+      Subject: "Email Verification OTP from Luxurious Creation",
+      Body: emailBody,
+    }).then(
+      message => {
+        if (message === "OK") {
+          setTimeout(() => {
+            alert("OTP sent to " + email.value)
+            otpbutton.innerText = "Resend OTP"
+          }, 2000);
+        } else {
+          alert("Error Occured!")
+        }
       }
-    }
-  );
+    );
+  }
 }
-
 
 // ======Verify OTP======
 function verifyMail() {
-  // let box = document.getElementById('messagebox');
   const otpInp = document.querySelector('[name="otp"]').value;
+  var isEmailVerified = false;
   console.log("verifyMail():" + otpVal);
   if (otpInp === otpVal) {
     alert("Email Address Verified Successfully.")
-    // box.removeAttribute('disabled');
+    isEmailVerified = true;
+  } else {
+    alert("Invalid OTP! Try Again");
   }
 
-  else {
-    alert("Invalid OTP! Try Again");
+  if (isEmailVerified) {
+    document.getElementById('submitButton').disabled = false;
+  } else {
+    // If email is not verified, disable the submit button
+    document.getElementById('submitButton').disabled = true;
   }
 }
 
