6d62d07818c8a9e5a4574123c794ba1798d32994
Pulkit verma <pulkit18012003@gmail.com>
2025-06-30 01:53:04 +0530

    feature2 designed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import NewItems from "./components/NewItems";
+import FeaturesSecond from "./components/FeaturesSecond";
 
 // import components
 
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <Features />
       <NewItems />
+      <FeaturesSecond />
     </div>
   );
 };
